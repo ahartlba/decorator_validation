64f7ef4e5b0f76a6a941ce4a9f5469bb19822601
Andreas Hartl-Bachinger <andreas.hartl-bachinger@gmx.at>
2024-09-11 16:44:28 +0200

    update __init__ imports

--- decorator_validation/__init__.py
+++ decorator_validation/__init__.py
@@ -1,4 +1,2 @@
 from .decorators import check_types, make_validator # noqa
 from .types import SkipTypeCheck # noqa
-import decorator_validation.types
-import decorator_validation.std_validators # noqa
